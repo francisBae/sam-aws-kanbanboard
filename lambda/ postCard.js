30d7d0cc5d45162f548a588be9134e8b83f46e70
frankie <jhbae@lgcns.com>
2022-04-12 17:01:56 +0900

    postCard aws lambda function

---
+++ lambda/ postCard.js
@@ -0,0 +1,42 @@
+var AWS = require("aws-sdk");
+var documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+});
+
+const tableName = "Cards";
+
+exports.handler = async (event) => {
+  console.log("Received: " + JSON.stringify(event, null, 2));
+  // TODO implement
+  let response = "";
+
+  try {
+    const id = event.requestContext.requestId; //cloudwatch 로그에 있는 request event 객체에 있는 정보 사용
+    const body = JSON.parse(event.body);
+
+    var params = {
+      TableName: tableName,
+      Item: {
+        id: id,
+        title: body.title,
+        category: body.category,
+      },
+    };
+
+    await documentClient.put(params).promise();
+
+    //api게이트웨이와 통합하기 위한 필수 속성들은 statusCode, body
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ id: id }),
+    };
+  } catch (exception) {
+    console.error(exception);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ "Message : ": exception }),
+    };
+  }
+
+  return response;
+};
